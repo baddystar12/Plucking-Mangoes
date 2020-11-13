076b886c252e174a319c5bf0b1a740e69610cdc7
baddystar12 <72180686+baddystar12@users.noreply.github.com>
2020-11-13 06:26:18 -0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -6,28 +6,33 @@ const Body = Matter.Body;
 const Constraint = Matter.Constraint;
 
 var mango1, mango2, mango3, mango4, mango5;
-var mangoImage1, mangoImage2, mangoImage3, mangoImage4, mangoImage5;
-var boy1, boyImage;
-var stone1, stoneImage;
-var tree1, treeImage;
+var boyImage;
+var stone1;
+var tree1;
+var ground;
+var elastic;
 
 function preload(){
 boyImage = loadImage("Plucking mangoes/boy.png");
-mangoImage1 = loadImage("Plucking mangoes/mango.png");
 }
 
 
 function setup() {
 	createCanvas(800, 700);
-boy1 = createSprite(50, 600, 50, 50);
-boy1.addImage(boyImage);
-boy1.scale = 0.1;
 
 	engine = Engine.create();
 	world = engine.world;
 
-	mango1 = new Mango(50, 50, 50, 50);
-
+	ground = new Ground(400, 695, 800, 20);
+	tree1 = new Tree(650, 515, 350);
+	mango1 = new Mango(560, 470, 40);
+	mango2 = new Mango(610, 420, 40);
+	mango3 = new Mango(670, 460, 40);
+	mango4 = new Mango(750, 455, 40);
+	mango5 = new Mango(700, 400, 40);
+	stone1 = new Stone(16, 569, 20);
+	//elastic = new Elastic(stone.body, {x:150, y:150});
+	
 
 	Engine.run(engine);
   
@@ -39,9 +44,17 @@ function draw() {
   background("turquoise");
   Engine.update(engine);
 
-  mango1.display();
   imageMode(CENTER);
-  image(mangoImage1, 50, 50, 50, 50);
+  image(boyImage, 60, 625, 150, 250); 
+  ground.display();
+  tree1.display();
+  mango1.display();
+  mango2.display();
+  mango3.display();
+  mango4.display();
+  mango5.display();
+  stone1.display();
+ //elastic.display();
   
   drawSprites();
  
