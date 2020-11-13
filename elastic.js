38fd61aa4b2bfa2f243d096aada6eccf7668069c
baddystar12 <72180686+baddystar12@users.noreply.github.com>
2020-11-13 06:24:16 -0800

    Create elastic.js

---
+++ elastic.js
@@ -0,0 +1,24 @@
+class Elastic {
+    constructor(bodyA, pointB){
+        var elastic_options ={
+            bodyA:bodyA,
+            pointB:pointB,
+            stiffness:0.04,
+            length:200
+        }
+        this.elastic = Matter.Constraint.create(elastic_options);
+        World.add(world, this.elastic);
+        this.pointB = pointB;
+    }
+    fly(){
+        this.elastic.bodyA = null;
+    }
+    display(){
+        if(this.elastic.bodyA){
+            var pointA = this.elastic.bodyA.position;
+            var pointB = this.pointB;
+            strokeWeight(3);
+            line(pointA.x, pointA.y, pointB.x, pointB.y);
+        }
+    }
+}
