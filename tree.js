bb5d98877a4187a44ee8f9ee8309a155b61ee402
baddystar12 <72180686+baddystar12@users.noreply.github.com>
2020-11-13 06:27:24 -0800

    Update tree.js

--- tree.js
+++ tree.js
@@ -1,19 +1,19 @@
 class Tree {
-    constructor(x,y){
+    constructor(x,y, radius){
         var tree_options ={
             friction:0.5,
             density:1,
-            restitution:1
+            restitution:0,
+            isStatic:true
         }
-        this.body = Bodies.rectangle(x,y, 50, 150, tree_options);
         this.image = loadImage("Plucking mangoes/tree.png");
-        this.height = 50;
-        this.width = 50;
+        this.radius = radius;
+        this.body = Matter.Bodies.circle(x,y,this.radius/2, tree_options);
         World.add(world, this.body);
     }
     display(){
         var position = this.body.position;
-        rectMode(CENTER);
-        rect(0,0, this.width, this.height);
+        imageMode(CENTER);
+        image(this.image, position.x, position.y, this.radius, this.radius);
     }
 }
