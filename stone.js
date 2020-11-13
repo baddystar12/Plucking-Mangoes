454c86f79977d999bcfea9db4e96c1db91dd90a5
baddystar12 <72180686+baddystar12@users.noreply.github.com>
2020-11-13 06:26:44 -0800

    Update stone.js

--- stone.js
+++ stone.js
@@ -1,20 +1,19 @@
 class Stone {
-    constructor(x,y){
+    constructor(x,y, radius){
 var stone_options={
     friction:1,
-    isStatic:false,
+    isStatic: true,
     restitution:0,
     density:1.2
 }
 this.image = loadImage("Plucking mangoes/stone.png");
-this.body = Bodies.rectangle(x,y,30,30, stone_options);
-this.height = 30;
-this.width = 30;
+this.radius = radius;
+this.body = Matter.Bodies.circle(x,y,this.radius/2, stone_options);
 World.add(world, this.body);
     }
     display(){
         var position = this.body.position;
-        rectMode(CENTER);
-        rect(0,0,this.width, this.height);
+        imageMode(CENTER);
+        image(this.image, position.x, position.y, this.radius, this.radius);
     }
 }
