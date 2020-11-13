5d23256b09cefb80bbda677fcf9cd1c17f3c7291
baddystar12 <72180686+baddystar12@users.noreply.github.com>
2020-11-13 06:25:49 -0800

    Update mango.js

--- mango.js
+++ mango.js
@@ -1,18 +1,18 @@
 class Mango {
-constructor(x,y,width, height){
+constructor(x,y,radius){
 var mango_options ={
-    isStatic: true,
     friction: 1,
+    isStatic:true,
     restitution:0
 }
-this.body = Bodies.rectangle(x,y,width,height,mango_options);
-this.width = width;
-this.height = height;
-World.add(world, this.mango);
+this.image = loadImage("Plucking mangoes/mango.png");
+this.radius = radius;
+this.body = Matter.Bodies.circle(x,y,this.radius/2, mango_options);
+World.add(world, this.body);
 }
 display(){
     var position = this.body.position;
-    rectMode(CENTER);
-    rect(position.x, position.y, this.width, this.height);
+    imageMode(CENTER);
+    image(this.image, position.x, position.y, this.radius, this.radius);
 }
 }
